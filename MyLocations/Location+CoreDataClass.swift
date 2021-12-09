754dc486a842c6b49c440d094e06df4d444fd3ee
nadya <kazynkina@keysystems.ru>
2021-12-09 14:32:55 +0300

    location map

--- MyLocations/Location+CoreDataClass.swift
+++ MyLocations/Location+CoreDataClass.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 import CoreData
+import MapKit
 
 @objc(Location)
-public class Location: NSManagedObject {
-
+public class Location: NSManagedObject, MKAnnotation {
+    
+    public var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(latitude, longitude)
+    }
+    
+    public var title: String? {
+        locationDescription.isEmpty ? "(No Description)" : locationDescription
+    }
+    
+    public var subtitle: String? {
+        return category
+    }
 }
